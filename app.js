7bf255983d037e0c2c06381baa210f9575da0cc5
Fectel <HectorGamador@gmail.com>
2024-08-17 22:03:11 +0000

    added webhook enpoint

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express')
 const fs = require('fs');
 const { Server } = require("socket.io");
 const https = require('https');
-
 const app = express()
 const port = process.env.PORT || 8000
 const http = require('http').Server(app) 
 const io = require('socket.io')(http) 
-
 
 // const server = https.createServer(
 //   {
@@ -34,8 +32,64 @@ io.on("connection", (socket) => {
 
 })
 // server.listen(8000)
-
-
 app.use(express.static('static'))
 http.listen(port)
-// app.listen([port])
+
+let transactionAmountInCents;
+let invoice_pdf;
+let invoice_number;
+const endpointSecret = "whsec_Q6OpSh5yLn9I0c2QMt4NbHT2hcAs58Zl";
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+
+  invoice_pdf = "";
+  invoice_number= 0;
+  transactionAmountInCents = 0;
+  console.log("INside /webhook!!!!");
+  // console.log("req.rwaBody" , request.rawBody)
+  // console.log("req.body", request.body);
+
+  let event;
+
+  try {
+      event = stripe.webhooks.constructEvent(request.body, request.headers['stripe-signature'], endpointSecret);
+  } catch (err) {
+      console.log(`Webhook Error: ${err.message}`);
+      return;
+  }
+
+  let  checkoutSessionAsyncPaymentSucceeded;
+  // Handle the event
+  switch (event.type) {
+          case 'invoice.payment_succeeded':
+           checkoutSessionAsyncPaymentSucceeded = event.data.object;
+          console.log('invoice.payment.succeeded')
+          transactionAmountInCents = checkoutSessionAsyncPaymentSucceeded.amount_paid
+          // hosted_invoice_url = checkoutSessionAsyncPaymentSucceeded.hosted_invoice_url
+          invoice_pdf = checkoutSessionAsyncPaymentSucceeded.invoice_pdf
+          // invoice_pdf = checkoutSessionAsyncPaymentSucceeded.hosted_invoice_url
+          invoice_number = checkoutSessionAsyncPaymentSucceeded.id
+
+          break;
+      default:
+     console.log(`Unhandled event type ${event.type}`);
+  }
+});
+// app.use(cors());
+app.use((req, res, next) => {
+    if (req.originalUrl === '/webhook' ) {
+        next()
+    }else {
+        bodyParser.json()(req, res, next);
+        // bodyParser.urlencoded({extended: true})(req, res, next);
+    }
+})
+app.use((req, res, next) => {
+  if (req.originalUrl === '/webhook') {
+      next()
+  }else {
+      bodyParser.urlencoded({ extended: true })(req, res, next);
+      // bodyParser.urlencoded({extended: true})(req, res, next);
+  }
+})
+
