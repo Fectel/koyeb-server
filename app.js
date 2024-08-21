f59332edc218f1e28d2a96c42f932160918d57ff
Fectel <HectorGamador@gmail.com>
2024-08-21 14:30:08 +0000

    added alter nater message and alos took out unrealized variable

--- app.js
+++ app.js
@@ -43,48 +43,19 @@ if (req.originalUrl === '/webhook') {
 }
 })
 
-io.on('connection', function (request) {
-  console.log("WS REQUEST Connection")
-  // if (pendingMemberId && pendingMembershipName && pendingMembershipId){
-      var userID = getUniqueID();
-      console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
-
-      // You can rewrite this part of the code to accept only the requests from allowed origin
-      const connection = request.accept(null, request.origin);
-      clients[userID] = connection;
-      console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
-
-      console.log(invoice_pdf, invoice_number, transactionAmountInCents, "invoice info")
-
-      let done = false;
-
-
-      console.log(invoice_number)
-      if (invoice_number === undefined){
-          for(key in clients) {
-              clients[key].send(transactionAmountInCents);
-
-          }
-      }else {
-          for(key in clients) {
-              clients[key].send(`${invoice_pdf}@${transactionAmountInCents}@${invoice_number}@${contractSignatureUrl}@${contractImgUrl}`);
-          }
-      }
-  // }
-});
-// server.listen(8000)
 app.use(cors());
 
-// app.use(express.static('static'))
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.static('static'))
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 http.listen(port)
 
 let transactionAmountInCents;
 let invoice_pdf;
 let invoice_number;
-const endpointSecret = "whsec_pJqGUEzEdLxovcQW0MAT6Impj5Q0pUDS";
+// const endpointSecret = "whsec_pJqGUEzEdLxovcQW0MAT6Impj5Q0pUDS";
+const endpointSecret = "whsec_sMVll0GkTzvuTo0q9MzFCXcj7UL6ZU5i";
 
 app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
 
@@ -118,9 +89,15 @@ app.post('/webhook', express.raw({type: 'application/json'}), (request, response
           invoice_pdf = checkoutSessionAsyncPaymentSucceeded.invoice_pdf
           // invoice_pdf = checkoutSessionAsyncPaymentSucceeded.hosted_invoice_url
           invoice_number = checkoutSessionAsyncPaymentSucceeded.id
+          io.emit('message', {invoice_pdf, invoice_number, transactionAmountInCents, contractSignatureUrl, contractImgUrl})
+          io.emit('message', `invoice_pdf: ${invoice_pdf}
+            invoice_number: ${invoice_number} 
+            transactionAmountInCents: ${transactionAmountInCents}
+            contractSignatureUrl: ${contractSignatureUrl}
+            contractImgUrl: ${contractImgUrl}`
+            )
 
           break;
-          io.emit('message', {invoice_pdf, invoice_number, transactionAmountInCents, contractSignatureUrl, contractImgUrl})
 
       default:
      console.log(`Unhandled event type ${event.type}`);
